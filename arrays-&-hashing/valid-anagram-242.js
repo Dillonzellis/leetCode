330abcd76427e698709b46420b7cbab365449d73
Dillon Ellis <dillonzellis@gmail.com>
2023-05-16 20:13:57 -0400

    solve anagram-242

--- arrays-&-hashing/valid-anagram-242.js
+++ arrays-&-hashing/valid-anagram-242.js
@@ -2,4 +2,20 @@
 
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
-var isAnagram = function (s, t) {};
+var isAnagram = function (s, t) {
+  sArr = Array.from(s);
+  tArr = Array.from(t);
+
+  sArr.sort();
+  tArr.sort();
+
+  if (sArr.length !== tArr.length) {
+    return false;
+  }
+
+  return sArr.every((e, i) => e === tArr[i]);
+
+  //   console.log(sArr, tArr);
+};
+
+console.log(isAnagram('csat', 'atc'));
